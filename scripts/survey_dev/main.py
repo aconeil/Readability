0dede6d2385f6e949792e12d7f41924f3382daf8
Alexandra <aconeil@iu.edu>
2022-11-29 14:05:37 -0500

    file_path_fix

--- scripts/survey_dev/main.py
+++ scripts/survey_dev/main.py
@@ -70,7 +70,8 @@ def lang():
 #               print(id)
 #               return flask.redirect(flask.url_for('lang', id=id))
         #each time the page is loaded it grabs two sentences from the languages tsv file
-        with open((os.getcwd()+"/../../sentences/"+id+".tsv"), "r") as in_file:
+        path = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(path, ("../../sentences/"+id+".tsv")), "r") as in_file:
                 data = in_file.readlines()
                 sentences = [x.split('\t') for x in data]
 # read in judgements from the database and produce "comparisons" datastructure
