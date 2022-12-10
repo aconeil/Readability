0b26b20a9bc80d407b2ac5c04327f058bd8de500
Alexandra <aconeil@iu.edu>
2022-12-09 19:21:09 -0500

    Working_survey

--- scripts/survey_dev/main.py
+++ scripts/survey_dev/main.py
@@ -1,24 +1,24 @@
-#import numpy
 import os
 import sqlite3
 import flask
 import random
 import ast
 
-sentences = []
-
-#from xbox import run_xbox
+from mle2 import mle
+from xbox import best_rankings
 
 from werkzeug.middleware.proxy_fix import ProxyFix
 app = flask.Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1, x_prefix=1, x_port=1)
-
+app.secret_key = "8763OCA"
 
 @app.route('/', methods=['GET', 'POST'])
+#participant is shown the irb approval form
 def cookie():
         return flask.render_template('index.html')
 
 @app.route('/setcookie', methods = ['GET', 'POST'])
+#if the participant approved the irb form, they can select a language
 def setcookie():
         if flask.request.method == 'POST':
                 resp = flask.make_response(flask.render_template('language_selection.html', iso = flask.request.args.get('iso')))
@@ -28,42 +28,49 @@ def setcookie():
                 return flask.redirect(flask.url_for('cookie'))
 
 @app.route('/getlang')
+#use language selection to pull up tsv file from lang and pass to lang route with iso code
 def getlang():
-        # sentences equal a random list of 100 integers in the range 0-10000
-        # sentences = numpy.random.randint(0, 10000, size=100)
-        # comparisons are minimum value of i and j and maximum value of i and j for sentences at index t in the list
-        # this should come from here
-        # comparisons is a list of pairs (i, j) where i is harder than j
-        # comparisons = [
-        #    (min(i,j,key=lambda t: sentences[t]),max(i,j, key=lambda t: sentences[t]))
-        #    #size equals 8 sets us to compare only 8 sentences?
-        #    for i in numpy.random.randint(0, len(sentences), size=8)
-        #    for j in numpy.random.randint(0, len(sentences), size=8) if i != j
-        # ]
-        path = os.path.dirname(os.path.abspath(__file__))
         iso = flask.request.args.get('iso')
-        in_file = open(os.path.join(path, ("../../sentences/" + iso + ".tsv")), "r")
-        data_bulk = in_file.readlines()
-        data = [x.split('\t') for x in data_bulk]
-        return sentenceselect(data, iso)
+        flask.session["ranking"] = load_data(iso)
+        return flask.redirect(flask.url_for('lang', iso=iso))
 
-def sentenceselect(data, iso):
-        sentpair = random.sample(data, k=2)
-        #print(sentpair)
-        return flask.render_template('ranking.html', sentence1=sentpair[0][2], sent1id=sentpair[0][0],
-                                     sentence2=sentpair[1][2], sent2id=sentpair[1][0], data=data, iso=iso)
+def load_sentences(iso):
+        path = os.path.dirname(os.path.abspath(__file__))
+        with open(os.path.join(path, ("../../sentences/" + iso + ".tsv")), 'r') as fin:
+                return [x.split('\t') for x in fin.readlines()]
+
+#load in the hundred sentences from last session or choose 100 random if first session ever
+def load_data(iso):
+        best_hundred = []
+        con = sqlite3.connect('results.db')
+        cur = con.cursor()
+        #if database table is empty, choose 100 random sentence pairs
+        value = cur.execute("SELECT * FROM " + iso + "nexthundred").fetchone()
+        if value == None:
+                list = random.sample(range(1000), 200)
+                for i in range(0, 200, 2):
+                        best_hundred.append([0, list[i], list[i+1]])
+        #else choose sentences pairs from database suggestions
+        else:
+                best_hundred = cur.execute("SELECT * FROM "+iso+"nexthundred").fetchall()
+                print("fetched from database")
+        return best_hundred
+
 @app.route('/lang', methods=['GET', 'POST'])
+#this function adds the judgements to the proper table in the database each time
 def lang():
         if flask.request.method == 'POST':
                 form = flask.request.form
+                iso = form['iso']
                 if form['easier'] == 'sent1id':
+                        judgement = (form['sentence2'], form['sentence1'])
                                 #this judgement should go into xbox df
                         # connect to database
                         con = sqlite3.connect('results.db')
                         # create cursor object
                         cur = con.cursor()
                         #append the judgement that sentence 1 is easier (i,j) where i is harder than j
-                        cur.execute("insert into judgements values (?, ?)", (("(" +form['sentence2'] + "," + form['sentence1'] + ")"), form['iso']))
+                        cur.execute("INSERT INTO "+iso+"judgements VALUES (?);", [','.join(judgement)])
                         con.commit()
                 else:
                         #save judgement to database as comparison
@@ -71,20 +78,56 @@ def lang():
                         # create cursor object
                         cur = con.cursor()
                         # append the judgement that sentence 2 is easier (i,j) where i is harder than j
-                        cur.execute("insert into judgements values (?, ?)", (("(" + form['sentence1'] + "," + form['sentence2'] + ")"), form['iso']))
+                        judgement = (form['sentence1'], form['sentence2'])
+                        cur.execute("INSERT INTO "+iso+"judgements VALUES (?);", [','.join(judgement)])
                         con.commit()
-                #this should be loaded in each time to update the comparisons
-                #comparisons = [(1,2), (3,2), (1,4)]
-                #this needs to return updated value for covariance and mean of each sentence and sorted list?
-                #run_xbox(sentences, comparisons)
-                data = ast.literal_eval(form['data'])
-        return sentenceselect(data, form['iso'])
+        else:
+                iso = flask.request.args.get('iso')
+        ranking_list = flask.session["ranking"]
+        #once user gets through sentences, show thanks screen
+        if not ranking_list:
+                return flask.render_template('thanks.html', iso=iso, data=load_sentences(iso))
+        data = load_sentences(iso)
+        ranking = ranking_list.pop()
+        flask.session["ranking"] = ranking_list
+        score, idone, idtwo = ranking[0], ranking[1], ranking[2]
+        # render ranking template using the selected ids
+        return flask.render_template('ranking.html', sentence1=data[idone][2], sent1id=data[idone][0],
+                              sentence2=data[idtwo][2], sent2id=data[idtwo][0], iso=iso)
 
-# each time the page is loaded it grabs two sentences from the language tsv file
-# read in judgements from the database and produce "comparisons" datastructure
-# run xbox with sentences + comparisons, storing the result in res
-# random sample with weighted coin flip from the covariance matrix for sentence--sentence (?)
-# this should be selected by xbox, currently it is taking any two random values in the data
+@app.route('/run_xbox', methods=['GET', 'POST'])
+def run_xbox():
+        form = flask.request.form
+        data = ast.literal_eval(form['data'])
+        iso = form['iso']
+        comparisons = []
+        sentences = []
+        #make a list of the sentences from data
+        for point in data:
+                sentences.append(point[2])
+        con = sqlite3.connect('results.db')
+        cur = con.cursor()
+        #open the language's database and get all the information
+        raw_comparisons = cur.execute("SELECT comparison FROM "+iso+"judgements").fetchall()
+        #append the comparisons from the file into list comparisons
+        for comparison in raw_comparisons:
+                csv_values = comparison[0]
+                values = csv_values.split(",")
+                values[0], values[1] = int(values[0]), int(values[1])
+                comparisons.append(values)
+        #pass sentences and comparisons to mle2 code to get means and covariance matrix
+        m, c = mle(comparisons, sentences)
+        print("finished mle part")
+        #get sorted list of best sentences to compare
+        xbox_choices = best_rankings(m, c)
+        print("finished xbox part")
+        #save this in db to open at beginning of next session
+        best_hundred = xbox_choices[:99]
+        #print(best_hundred)
+        for ranking in best_hundred:
+                cur.execute("INSERT INTO "+iso+"nexthundred VALUES (?, ?, ?);", (ranking[0], ranking[1], ranking[2]))
+                con.commit()
+        return flask.render_template("index.html")
 
 if __name__  == "__main__":
         app.run(debug=True)
---
+++ scripts/survey_dev/mle2.py
@@ -0,0 +1,127 @@
+import datetime
+import sys
+import numpy
+import scipy.stats
+import scipy.optimize
+
+# sentences = numpy.random.randint(0, 10000, size=100)
+# comparisons = [
+#     (min(i,j,key=lambda t: sentences[t]),max(i,j, key=lambda t: sentences[t]))
+#     for i in numpy.random.randint(0, len(sentences), size=8)
+#     for j in numpy.random.randint(0, len(sentences), size=8) if i != j
+# ]
+#
+# print(comparisons)
+
+def mle(comparisons, sentences):
+    means = numpy.zeros(len(sentences))
+    #means = numpy.array([0,2,1,3])
+    # truth = sentences
+    # print("truth: %r" % truth)
+    cov = numpy.identity(len(sentences))
+
+    a = numpy.pi / 4
+    rot = numpy.array([[numpy.cos(a), numpy.sin(a)], [-numpy.sin(a), numpy.cos(a)]])
+    rot_T = numpy.transpose(rot)
+
+    def rotate_cov(c):
+        return rot_T @ c @ rot
+
+    def rotate_mean(m):
+        return rot_T @ m
+
+    def logprob_comparison(m, c):
+        try:
+            p = scipy.stats.multivariate_normal.logcdf(numpy.array((0, numpy.inf)), mean=rotate_mean(m), cov=rotate_cov(c))
+            return p
+        except Exception as e:
+            return numpy.inf
+
+    def logprob_comparisons(comparisons, means, cov):
+        p = sum(logprob_comparison(project(means,comparison), project(cov, comparison)) for comparison in comparisons )
+        return p
+    def project(x, indices):
+        if len(x.shape) == 1:
+            return numpy.array([x[i] for i in indices])
+        else:
+            return numpy.array([project(x[i], indices) for i in indices])
+
+    # extract only the upper triangular entries
+    def to_ut(c):
+        return numpy.array([c[i, j] for i in range(len(c)) for j in range(len(c)) if i <= j])
+
+    def from_ut(x):
+        ind = [(i, j) for i in range(len(cov)) for j in range(len(cov)) if i <= j]
+        return numpy.array([[ x[ind.index((min(i, j),max(i,j)))] for i in range(len(cov))] for j in range(len(cov))])
+
+    def objective(x, comparisons):
+        return -logprob_comparisons(
+            comparisons,
+            (( x[:len(sentences)])),
+            numpy.diag((( x[len(sentences):])))
+        )
+
+    def unpack_res(res):
+        return (( res.x[:len(sentences)])), numpy.diag((( res.x[len(sentences):])))
+
+    dt = datetime.datetime.now()
+    print("starting base at %s" % dt)
+    res = scipy.optimize.minimize(
+        objective,
+        numpy.concatenate((means, numpy.diagonal(cov))),
+        args=(comparisons[:-1],),
+        options={"ftol":1e-10},
+        bounds=scipy.optimize.Bounds(
+            lb=0,
+            ub=numpy.inf
+        )
+    )
+    #this is the mean and covariance matrix
+    m, c = unpack_res(res)
+
+    return m, c
+
+# dt = datetime.datetime.now()
+# print("starting incremental at %s" % dt)
+#
+# res2 = scipy.optimize.minimize(
+#     objective,
+#     numpy.concatenate((m, numpy.diagonal(c))),
+#     args=(comparisons,),
+#     options={"ftol":1e-10},
+#     bounds=scipy.optimize.Bounds(
+#         lb=0,
+#         ub=numpy.inf
+#     )
+# )
+#
+# dt2 = datetime.datetime.now()
+# print("finished incremental at %s" % dt)
+#
+# print(dt2 - dt)
+#
+# dt = datetime.datetime.now()
+# print("starting full at %s" % dt)
+# res2 = scipy.optimize.minimize(
+#     objective,
+#     numpy.concatenate((m, numpy.diagonal(c))),
+#     args=(comparisons,),
+#     options={"ftol":1e-10},
+#     bounds=scipy.optimize.Bounds(
+#         lb=0,
+#         ub=numpy.inf
+#     )
+# )
+#
+# dt2 = datetime.datetime.now()
+# print("finished full at %s" % dt2)
+# print(dt2 - dt)
+# print(res2)
+#
+#
+# #for c in comparisons:
+# #    print(m[c[0]] < m[c[1]])
+# #    if m[c[0]] >= m[c[1]]:
+# #        print("ugh %r is not less than %r" % (m[c[0]], m[c[1]]))
+# #        print("but truth says they should be: %r < %r" % (truth[c[0]], truth[c[1]]))
+# #
